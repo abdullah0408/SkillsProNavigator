2f5186c60474b0c31507d509c28c46d87fe4ac03
abdullah <abdullah981066@gmail.com>
2025-04-23 20:12:41 +0530

    Update quiz generation to create 10 technical interview questions instead of 3

--- actions/interview.js
+++ actions/interview.js
@@ -20,7 +20,7 @@ export async function generateQuiz(){
 
       //prompt to generate quiz 
     const prompt = `
-    Generate 3 technical interview questions for a ${
+    Generate 10 technical interview questions for a ${
       user.industry
     } professional${
     user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
